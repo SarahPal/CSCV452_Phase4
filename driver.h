023b179f6015e23089d8089065fb8818ad4cab0c
SarahPal <47961885+SarahPal@users.noreply.github.com>
2019-04-25 11:01:38 -0700

    Update driver.h

--- driver.h
+++ driver.h
@@ -1,6 +1,9 @@
+#define DEBUG4 1
 
-
+typedef struct driver_proc driver_proc;
 typedef struct driver_proc * driver_proc_ptr;
+typedef struct proc_struct proc_struct;
+typedef struct proc_struct * proc_ptr;
 
 struct driver_proc {
    driver_proc_ptr next_ptr;
@@ -18,5 +21,34 @@ struct driver_proc {
 
    //more fields to add
 
+};
+
+struct proc_struct {
+   short          pid;               /* process id */
+   short          ppid;
+   char           name[MAXNAME];
+   char           startArg[MAXARG];
+   int            priority;
+   int (* start_func) (char *);
+   int            stack_size;
+   int            spawnBox;
+   int            num_children;
+   proc_ptr       next;
+   proc_ptr       childProcPtr;
+   proc_ptr       nextSiblingPtr;
+   /* other fields as needed... */
+};
+
+struct psr_bits {
+    unsigned int cur_mode:1;
+    unsigned int cur_int_enable:1;
+    unsigned int prev_mode:1;
+    unsigned int prev_int_enable:1;
+    unsigned int unused:28;
+};
+
+union psr_values {
+   struct psr_bits bits;
+   unsigned int integer_part;
 };
 
